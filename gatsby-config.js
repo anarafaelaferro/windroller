6ecf1f09398af3f625dad4b42a7483826af0c001
Rafaela <anarafaelaferro@gmail.com>
2025-01-03 19:17:08 +0000

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Windroller",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Windroller is a company that manufactures land sailers, aiming to provide outdoor sports enthusiasts with a vehicle that combines the pleasure of sailing with land mobility.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
