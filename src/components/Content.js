d89176a10ad077bcc65ed13996141256a90123d5
Rafaela <anarafaelaferro@gmail.com>
2025-01-03 18:13:50 +0000

    update /about content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,14 @@
 import * as React from "react";
 import PropTypes from "prop-types";
+import { marked } from 'marked';
 
-export const HTMLContent = ({ content, className }) => (
-  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
-);
+export const HTMLContent = ({ content, className }) => {
+  const contentHtml = marked(content || '');
+
+  return (
+    <div className={className} dangerouslySetInnerHTML={{ __html: contentHtml }} />
+  )
+};
 
 const Content = ({ content, className }) => (
   <div className={className}>{content}</div>
